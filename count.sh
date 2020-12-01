a22f72fe0151988f945a6963326f07fbd23448b1
ruoyu.chen <ruoyu.chen@puzzleplusgames.com>
2020-12-01 20:15:11 +0800

    update

--- count.sh
+++ count.sh
@@ -1,7 +1,9 @@
-grep -o 'stage":"初筛' text | wc -l
-grep -o 'stage":"用人部门筛选' text | wc -l
-grep -o 'stage":"初试' text | wc -l
-grep -o 'stage":"复试' text | wc -l
-grep -o 'stage":"HR面试' text | wc -l
-grep -o 'stage":"pending' text | wc -l
-grep -o 'stage":"沟通offer' text | wc -l
+grep -o 'status":"初筛' text | wc -l
+grep -o 'status":"用人部门筛选' text | wc -l
+grep -o 'status":"初试' text | wc -l
+grep -o 'status":"复试' text | wc -l
+grep -o 'status":"HR面试' text | wc -l
+grep -o 'status":"pending' text | wc -l
+grep -o 'status":"暂不匹配' text | wc -l
+grep -o 'status":"沟通offer' text | wc -l
+grep -o '.\{162\}status":"沟通offer.\{148\}' text
